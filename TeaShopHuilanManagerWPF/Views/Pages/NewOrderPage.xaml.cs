f93e55887ef7155d7424bf9aab386dd6b0eee7c5
машуша розми <irozina2006@gmail.com>
2025-11-10 07:50:39 +1000

    Куча окошек хы

---
+++ TeaShopHuilanManagerWPF/Views/Pages/NewOrderPage.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TeaShopHuilanManagerWPF.Views.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для NewOrderPage.xaml
+    /// </summary>
+    public partial class NewOrderPage : Page
+    {
+        public NewOrderPage()
+        {
+            InitializeComponent();
+        }
+
+        private void MakePrecheck(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void AddOneMore(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void DeleteOneItem(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BindGuest(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void AddDiscount(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MakePayment(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
