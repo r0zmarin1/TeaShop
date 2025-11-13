cb3273e6d9b2eeebb2fb9212dd16429db896d853
Denoos <danil2006timofeev@gmail.com>
2025-11-13 23:14:54 +1000

    Сервис конвертации полностью завершен

--- TeaShopHuilanDatabaseApi/Core/Controllers/Services/ModelsConverterService.cs
+++ TeaShopHuilanDatabaseApi/Core/Controllers/Services/ModelsConverterService.cs
@@ -2,6 +2,8 @@
 {
     public static class ModelsConverterService
     {
+        #region FromEftoPdo
+
         public static async Task<List<Models.DTOs.Booking>> FromEfToPdo(List<Models.EfModels.Booking> items)
         {
             List<Models.DTOs.Booking> result = [];
@@ -157,7 +159,7 @@
                 TimeStamp = item.TimeStamp,
 
                 User = await FromEfToPdo(item.IdUserNavigation),
-               Type = await FromEfToPdo(item.IdReportTypeNavigation)
+                Type = await FromEfToPdo(item.IdReportTypeNavigation)
             };
 
             return result;
@@ -289,5 +291,299 @@
 
             return result;
         }
+
+        #endregion
+
+        #region FromPdoToEf
+
+        public static async Task<List<Models.EfModels.Booking>> FromPdoToEf (List<Models.DTOs.Booking> items)
+        {
+            List<Models.EfModels.Booking> result = [];
+
+            foreach (var item in items)
+            {
+                result.Add(await FromPdoToEf(item));
+            }
+
+            return result;
+        }
+
+        public static async Task<Models.EfModels.Booking> FromPdoToEf(Models.DTOs.Booking item)
+        {
+            Models.EfModels.Booking result = new()
+            {
+                Id = item.Id,
+                TimeStamp = item.TimeStamp,
+                IdUser = item.UserId,
+                IdTable = item.TableId,
+                HoursCount = item.HoursCount,
+
+                IdUserNavigation = await FromPdoToEf(item.User),
+                IdTableNavigation = await FromPdoToEf(item.Table)
+            };
+
+            return result;
+        }
+
+        public static async Task<List<Models.EfModels.Category>> FromPdoToEf(List<Models.DTOs.Category> items)
+        {
+            List<Models.EfModels.Category> result = [];
+
+            foreach (var item in items)
+            {
+                result.Add(await FromPdoToEf(item));
+            }
+
+            return result;
+        }
+
+        public static async Task<Models.EfModels.Category> FromPdoToEf(Models.DTOs.Category item)
+        {
+            Models.EfModels.Category result = new()
+            {
+                Id = item.Id,
+                Title = item.Title
+            };
+
+            return result;
+        }
+
+        public static async Task<List<Models.EfModels.Order>> FromPdoToEf(List<Models.DTOs.Order> items)
+        {
+            List<Models.EfModels.Order> result = [];
+
+            foreach (var item in items)
+            {
+                result.Add(await FromPdoToEf(item));
+            }
+
+            return result;
+        }
+
+        public static async Task<Models.EfModels.Order> FromPdoToEf(Models.DTOs.Order item)
+        {
+            Models.EfModels.Order result = new()
+            {
+                Id = item.Id,
+                Cost = item.Cost,
+                IdBooking = item.BookingId,
+
+                IdBookingNavigation = await FromPdoToEf(item.Booking)
+            };
+
+            return result;
+        }
+
+        public static async Task<List<Models.EfModels.Product>> FromPdoToEf(List<Models.DTOs.Product> items)
+        {
+            List<Models.EfModels.Product> result = [];
+
+            foreach (var item in items)
+            {
+                result.Add(await FromPdoToEf(item));
+            }
+
+            return result;
+        }
+
+        public static async Task<Models.EfModels.Product> FromPdoToEf(Models.DTOs.Product item)
+        {
+            Models.EfModels.Product result = new()
+            {
+                Id = item.Id,
+                Title = item.Title,
+                Cost = item.Cost,
+                IdCategory = item.CategoryId,
+
+                IdCategoryNavigation = await FromPdoToEf(item.Category)
+            };
+
+            return result;
+        }
+
+        public static async Task<List<Models.EfModels.Productslist>> FromPdoToEf(List<Models.DTOs.ProductList> items)
+        {
+            List<Models.EfModels.Productslist> result = [];
+
+            foreach (var item in items)
+            {
+                result.Add(await FromPdoToEf(item));
+            }
+
+            return result;
+        }
+
+        public static async Task<Models.EfModels.Productslist> FromPdoToEf(Models.DTOs.ProductList item)
+        {
+            Models.EfModels.Productslist result = new()
+            {
+                IdOrder = item.OrderId,
+                IdProduct = item.ProductId,
+                ProductCount = item.ProductCount,
+
+                IdOrderNavigation = await FromPdoToEf(item.Order),
+                IdProductNavigation = await FromPdoToEf(item.Product)
+            };
+
+            return result;
+        }
+
+        public static async Task<List<Models.EfModels.Report>> FromPdoToEf(List<Models.DTOs.Report> items)
+        {
+            List<Models.EfModels.Report> result = [];
+
+            foreach (var item in items)
+            {
+                result.Add(await FromPdoToEf(item));
+            }
+
+            return result;
+        }
+
+        public static async Task<Models.EfModels.Report> FromPdoToEf(Models.DTOs.Report item)
+        {
+            Models.EfModels.Report result = new()
+            {
+                Id = item.Id,
+                IdUser = item.UserId,
+                IdReportType = item.ReportTypeId,
+                Path = item.Path,
+                TimeStamp = item.TimeStamp,
+
+                IdUserNavigation = await FromPdoToEf(item.User),
+                IdReportTypeNavigation = await FromPdoToEf(item.Type)
+            };
+
+            return result;
+        }
+
+        public static async Task<List<Models.EfModels.Reporttype>> FromPdoToEf(List<Models.DTOs.ReportType> items)
+        {
+            List<Models.EfModels.Reporttype> result = [];
+
+            foreach (var item in items)
+            {
+                result.Add(await FromPdoToEf(item));
+            }
+
+            return result;
+        }
+
+        public static async Task<Models.EfModels.Reporttype> FromPdoToEf(Models.DTOs.ReportType item)
+        {
+            Models.EfModels.Reporttype result = new()
+            {
+                Id = item.Id,
+                Title = item.Title
+            };
+
+            return result;
+        }
+
+        public static async Task<List<Models.EfModels.Role>> FromPdoToEf(List<Models.DTOs.Role> items)
+        {
+            List<Models.EfModels.Role> result = [];
+
+            foreach (var item in items)
+            {
+                result.Add(await FromPdoToEf(item));
+            }
+
+            return result;
+        }
+
+        public static async Task<Models.EfModels.Role> FromPdoToEf(Models.DTOs.Role item)
+        {
+            Models.EfModels.Role result = new()
+            {
+                Id = item.Id,
+                Title = item.Title
+            };
+
+            return result;
+        }
+
+        public static async Task<List<Models.EfModels.Status>> FromPdoToEf(List<Models.DTOs.Status> items)
+        {
+            List<Models.EfModels.Status> result = [];
+
+            foreach (var item in items)
+            {
+                result.Add(await FromPdoToEf(item));
+            }
+
+            return result;
+        }
+
+        public static async Task<Models.EfModels.Status> FromPdoToEf(Models.DTOs.Status item)
+        {
+            Models.EfModels.Status result = new()
+            {
+                Id = item.Id,
+                Title = item.Title
+            };
+
+            return result;
+        }
+
+        public static async Task<List<Models.EfModels.Table>> FromPdoToEf(List<Models.DTOs.Table> items)
+        {
+            List<Models.EfModels.Table> result = [];
+
+            foreach (var item in items)
+            {
+                result.Add(await FromPdoToEf(item));
+            }
+
+            return result;
+        }
+
+        public static async Task<Models.EfModels.Table> FromPdoToEf(Models.DTOs.Table item)
+        {
+            Models.EfModels.Table result = new()
+            {
+                Id = item.Id,
+                IdStatus = item.StatusId,
+                MaxPeopleCount = item.MaxPeopleCount,
+
+                IdStatusNavigation = await FromPdoToEf(item.Status)
+            };
+
+            return result;
+        }
+
+        public static async Task<List<Models.EfModels.User>> FromPdoToEf(List<Models.DTOs.User> items)
+        {
+            List<Models.EfModels.User> result = [];
+
+            foreach (var item in items)
+            {
+                result.Add(await FromPdoToEf(item));
+            }
+
+            return result;
+        }
+
+        public static async Task<Models.EfModels.User> FromPdoToEf(Models.DTOs.User item)
+        {
+            Models.EfModels.User result = new()
+            {
+                Id = item.Id,
+                FirstName = item.FirstName,
+                LastName = item.LastName,
+                Patronymic = item.Patronymic,
+                IsBlocked = (sbyte)(item.IsBlocked ? 1 : 0),
+                IdRole = item.RoleId,
+                BonusesCount = item.BonusesCount,
+                Password = item.Password,
+                PhoneNumber = item.PhoneNumber,
+
+                IdRoleNavigation = await FromPdoToEf(item.Role)
+            };
+
+            return result;
+        }
+
+        #endregion
     }
 }
