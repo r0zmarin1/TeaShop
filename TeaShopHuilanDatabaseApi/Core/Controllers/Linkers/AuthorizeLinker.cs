a721219cd7e1eccc37dfc1a1ccf137cc9403f069
Denoos <danil2006timofeev@gmail.com>
2025-11-14 21:21:34 +1000

    Завершена авторизация на стороне сервера

---
+++ TeaShopHuilanDatabaseApi/Core/Controllers/Linkers/AuthorizeLinker.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TeaShopHuilanDatabaseApi.Core.Controllers.Services;
+using TeaShopHuilanDatabaseApi.Core.Models.DTOs;
+
+namespace TeaShopHuilanDatabaseApi.Core.Controllers.Linkers
+{
+    public class AuthorizeLinker
+    {
+        private RequestService _requester = RequestService.Instance;
+
+        public async Task<User> Authorize(User user)
+        {
+            if (ValidationService.CheckValidness(user))
+            {
+                var item = await _requester.Authorize(await ModelsConverterService.FromPdoToEf(user));
+                return await ModelsConverterService.FromEfToPdo(item);
+            }
+
+            return null;
+        }
+    }
+}
---
+++ TeaShopHuilanDatabaseApi/Core/Controllers/Views/AuthorizeView.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TeaShopHuilanDatabaseApi.Core.Controllers.Linkers;
+using TeaShopHuilanDatabaseApi.Core.Models.DTOs;
+
+namespace TeaShopHuilanDatabaseApi.Core.Controllers.Views
+{
+    [Route("api/Authorize")]
+    [ApiController]
+    public class AuthorizeView : ControllerBase
+    {
+        private AuthorizeLinker _linker;
+
+        public AuthorizeView()
+        {
+            _linker = new();
+        }
+
+        [HttpGet("Authorize")]
+        public async Task<ActionResult<User>> GetRoles(User item)
+        {
+            var result = await _linker.Authorize(item);
+            if (result == null)
+                return NoContent();
+
+            return result;
+        }
+    }
+}
